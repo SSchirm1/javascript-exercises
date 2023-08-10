571c95ddc180f940960d50bdb5565517cae9ed3b
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-10 16:03:57 +0200

    task 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number") {
+        return ("ERROR");
+    }
+
+    var sum = 0;
+    for (i = Math.min(a,b); i <= Math.max(a,b); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
