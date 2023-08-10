d2f23ee26df3e893122ee12c8b9e8aa30afb80e2
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-10 22:26:21 +0200

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        return true;        
+    }
+    return false;
 };
 
+
+console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
+
+// 1. check if divisible by two 
+// it should not be divisible by 100, unless it is also divisible by 400)
