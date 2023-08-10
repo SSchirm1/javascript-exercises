0f48b41118340d34f31d7bac2778f2bf6e76c54d
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-10 15:37:10 +0200

    task 4 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ... elements) {
 
+    let elementsToRemove = [...elements];
+    console.log(elementsToRemove);
+
+    const filteredArray = arr.filter((element) => elementsToRemove.indexOf(element) < 0);
+    console.log("filtered: ", filteredArray);
+
+    return filteredArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
