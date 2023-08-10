bb4b41156889a5ec4d29f31aa58808c8d99960db
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-10 14:36:17 +0200

    task 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    repeatedString = "";
+    if (parseInt(num) < 0) {
+        repeatedString = 'ERROR';
+    }
+    else {
+        for (i = 1; i <= num; i++) {
+            repeatedString += string;
+        }
+      
+    }
+    return repeatedString;
+    
 
 };
+
+repeatString("hey", -1)
 
 // Do not edit below this line
 module.exports = repeatString;
