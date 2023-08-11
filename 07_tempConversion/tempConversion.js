feae167d60ac071f46fa383a657585f5a2a91988
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-11 10:03:54 +0200

    Solved exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celcius = (fahrenheit - 32) * 5 / 9;
+  if (celcius % 1 != 0) {
+    return +celcius.toFixed(1)
+  }
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  fahrenheit = (celcius * 9 / 5) + 32;
+  if (fahrenheit % 1 != 0) {
+    return +fahrenheit.toFixed(1);
+  }
+  return fahrenheit;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
