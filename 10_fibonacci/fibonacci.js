eac632a5b444aee0d6246c9e7d5bc91da85cf25e
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-14 15:09:28 +0200

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    let fibArray = [1,1];
+    for(let i = 2; i < number; i++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2]; 
+    }
+    return fibArray[number-1]
 };
 
+fibonacci(5);
 // Do not edit below this line
 module.exports = fibonacci;
