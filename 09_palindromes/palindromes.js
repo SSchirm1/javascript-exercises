af5e7d15563876eede5fb4e0067457be4965e739
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-14 14:40:36 +0200

    Completed Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringToLettersOnly = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+    let reversedString = stringToLettersOnly.split("").reverse().join("");
+
+    console.log(stringToLettersOnly);
+    console.log(reversedString);
+
+    return reversedString == stringToLettersOnly;
+  
 };
+
+palindromes("hello world");
+palindromes("idjri,.oodk  e w");
 
 // Do not edit below this line
 module.exports = palindromes;
