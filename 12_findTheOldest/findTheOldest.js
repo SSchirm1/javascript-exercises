61e4867758c0c664b6337f8cb8c8751ee312ceb8
Sondre Schirmer-Mikalsenclearls <sondremikalsen1@gmail.com>
2023-08-14 15:37:23 +0200

    completed ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
 
+
+    return people.reduce((a, b) => ((a.yearOfDeath ? a.yearOfDeath : currentYear) - a.yearOfBirth)
+     > ((b.yearOfDeath ? b.yearOfDeath : currentYear) - b.yearOfBirth) ? a : b);
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
